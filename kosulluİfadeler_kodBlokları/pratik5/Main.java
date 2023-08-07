8aa48cf0e84854ea47f2fb1509a95c366d27cba9
Özge Ünal <67192793+augustada@users.noreply.github.com>
2023-08-07 17:20:12 +0300

    commit

---
+++ kosulluİfadeler_kodBlokları/pratik5/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        int a,b,c;
+        Scanner inp=new Scanner(System.in);
+        System.out.print("1.Sayı: ");
+        a=inp.nextInt();
+        System.out.print("2.Sayı: ");
+        b=inp.nextInt();
+        System.out.print("3.Sayı: ");
+        c=inp.nextInt();
+
+        if (a>b && a>c){
+            if(b>c){
+                System.out.println(a+">"+b+">"+c);
+            } else {
+                System.out.println(a+">"+c+">"+b);
+            }
+        }
+        if (b>a && b>c){
+            if(a>c){
+                System.out.println(b+">"+a+">"+c);
+            } else {
+                System.out.println(b+">"+c+">"+a);
+            }
+        }
+        if (c>b && c>a){
+            if(b>a){
+                System.out.println(c+">"+b+">"+a);
+            } else {
+                System.out.println(c+">"+a+">"+b);
+            }
+        }
+
+    }
+}
