548489be4582b117e981b0a4243aa54399f763f5
Özge Ünal <67192793+augustada@users.noreply.github.com>
2023-08-07 18:23:50 +0300

    commit

---
+++ kosulluİfadeler_kodBlokları/odev2/Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        int yil,burc;
+        Scanner inp=new Scanner(System.in);
+        System.out.print("Doğum Yılınızı Girin: ");
+        yil=inp.nextInt();
+        burc=yil%12;
+        switch (burc){
+            case 0:
+                System.out.print("Maymun");
+                break;
+            case 1:
+                System.out.print("Horoz");
+                break;
+            case 2:
+                System.out.print("Köpek");
+                break;
+            case 3:
+                System.out.print("Domuz");
+                break;
+            case 4:
+                System.out.print("Fare");
+                break;
+            case 5:
+                System.out.print("Öküz");
+                break;
+            case 6:
+                System.out.print("Kaplan");
+                break;
+            case 7:
+                System.out.print("Tavşan");
+                break;
+            case 8:
+                System.out.print("Ejderha");
+                break;
+            case 9:
+                System.out.print("Yılan");
+                break;
+            case 10:
+                System.out.print("At");
+                break;
+            case 11:
+                System.out.print("Koyun");
+                break;
+        }
+    }
+}
