d6679a20b141199009491f8b2460512720e81a9c
Özge Ünal <67192793+augustada@users.noreply.github.com>
2023-08-20 21:00:10 +0300

    commit

---
+++ diziler/odev2/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] list={10, 20, 20, 10, 10, 20, 5, 20,0};
+        for(int j = 0 ; j < list.length ; j++) {
+            int count = 0;
+            boolean isCount = false;
+
+            for (int k = j - 1; k >= 0; --k) {
+                if (list[k] == list[j]) {
+                    isCount = true;
+                    break;
+                }
+            }
+            if (!isCount) {
+                for (int i = 0; i < list.length; i++) {
+                    if (list[j] == list[i]) count++;
+                }
+
+
+                System.out.println(list[j] + " sayısı " + count+" kere tekrar edildi.");
+            }
+        }
+        
+        
+        
+    }
+}
